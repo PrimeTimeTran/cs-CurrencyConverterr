01314b61f200d032779a85ef70f6cfa538b4d8cf
Loi Tran <datloiboi@gmail.com>
2020-02-13 23:57:14 +0700

    Add reverse and title

--- script.js
+++ script.js
@@ -1,24 +1,50 @@
 function formatCurrency(currency, value) {
   const formatter = new Intl.NumberFormat(currency, {
     currency: currency,
-    style: 'currency'
+    style: "currency"
   });
-  return formatter.format(value)
-};
+  return formatter.format(value);
+}
+
+function updateTitle(converted, value, from, to) {
+  "100,000 VND to USD = 4.30314 US Dollars";
+  const prompt = `${formatCurrency(from, value)} ${from} to ${to} = ${converted} ${to}`
+  document.getElementById("prompt").innerHTML = prompt;
+}
 
 function convertCurrency() {
   const from = document.getElementById("from").value;
   const to = document.getElementById("to").value;
   const amount = document.getElementById("amount").value;
 
+  const initialAmountAndCurrency = `${amount} ${from} =`;
+
   if (from === "USD") {
-    const value = amount * 23000
-    document.getElementById("converted-amount").innerHTML = formatCurrency(to, value);
-    document.getElementById('start-amount').innerHTML = `${amount} ${from} =`
+    const value = amount * 23000;
+    const convertedAmount = formatCurrency(to, value);
+
+    document.getElementById("converted-amount").innerHTML = convertedAmount;
+    document.getElementById(
+      "start-amount"
+    ).innerHTML = initialAmountAndCurrency;
+    updateTitle(convertedAmount, amount, from, to);
   }
-  if (from === "VND" && to === 'USD') {
-    const value = amount / 23000
-    document.getElementById("converted-amount").innerHTML = formatCurrency(to, value);
-    document.getElementById('start-amount').innerHTML = `${amount} ${from} =`
+  if (from === "VND" && to === "USD") {
+    const value = amount / 23000;
+    const convertedAmount = formatCurrency(to, value);
+
+    document.getElementById("converted-amount").innerHTML = convertedAmount;
+    document.getElementById(
+      "start-amount"
+    ).innerHTML = initialAmountAndCurrency;
+    updateTitle(convertedAmount, amount, from, to);
   }
+}
+
+function reverse() {
+  const from = document.getElementById("from").value;
+  const to = document.getElementById("to").value;
+
+  document.getElementById("from").value = to;
+  document.getElementById("to").value = from;
 }
