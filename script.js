ebdb2a78dc8544e49b0347f5a3f11a86e6616fa6
Loi Tran <datloiboi@gmail.com>
2020-02-13 16:35:53 +0700

    Add format currency and update start ampount

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
+function formatCurrency(currency, value) {
+  const formatter = new Intl.NumberFormat(currency, {
+    currency: currency,
+    style: 'currency'
+  });
+  return formatter.format(value)
+};
+
 function convertCurrency() {
   const from = document.getElementById("from").value;
   const to = document.getElementById("to").value;
   const amount = document.getElementById("amount").value;
 
   if (from === "USD") {
-    document.getElementById("converted-amount").innerHTML = amount * 23000;
+    const value = amount * 23000
+    document.getElementById("converted-amount").innerHTML = formatCurrency(to, value);
+    document.getElementById('start-amount').innerHTML = `${amount} ${from} =`
   }
 }
