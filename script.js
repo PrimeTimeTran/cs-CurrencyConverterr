9fa47e68e47309dbc328db91fc143aef4c81ecd1
Loi Tran <datloiboi@gmail.com>
2020-02-13 12:44:30 +0700

    Convert Currency Fires when clicked

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-console.log('Im Javascript code running in your currency converter')
+function convertCurrency() {
+  console.log("Ok I'll begin converting currency now");
+
+  // 🔑🔒Let's get the from currency
+  const from = document.getElementById("from").value;
+  const to = document.getElementById("to").value;
+  console.log({ from: from, to: to });
+}
