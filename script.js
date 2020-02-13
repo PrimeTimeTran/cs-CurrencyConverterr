8e4683ab1d4ca71897bc7c711205c0d0baae509c
Loi Tran <datloiboi@gmail.com>
2020-02-14 00:05:30 +0700

    Refactored JS

--- script.js
+++ script.js
@@ -8,7 +8,10 @@ function formatCurrency(currency, value) {
 
 function updateTitle(converted, value, from, to) {
   "100,000 VND to USD = 4.30314 US Dollars";
-  const prompt = `${formatCurrency(from, value)} ${from} to ${to} = ${converted} ${to}`
+  const prompt = `${formatCurrency(
+    from,
+    value
+  )} ${from} to ${to} = ${converted} ${to}`;
   document.getElementById("prompt").innerHTML = prompt;
 }
 
@@ -16,29 +19,20 @@ function convertCurrency() {
   const from = document.getElementById("from").value;
   const to = document.getElementById("to").value;
   const amount = document.getElementById("amount").value;
-
   const initialAmountAndCurrency = `${amount} ${from} =`;
 
-  if (from === "USD") {
-    const value = amount * 23000;
-    const convertedAmount = formatCurrency(to, value);
+  let value;
 
-    document.getElementById("converted-amount").innerHTML = convertedAmount;
-    document.getElementById(
-      "start-amount"
-    ).innerHTML = initialAmountAndCurrency;
-    updateTitle(convertedAmount, amount, from, to);
+  if (from === "USD") {
+    value = amount * 23000;
   }
   if (from === "VND" && to === "USD") {
-    const value = amount / 23000;
-    const convertedAmount = formatCurrency(to, value);
-
-    document.getElementById("converted-amount").innerHTML = convertedAmount;
-    document.getElementById(
-      "start-amount"
-    ).innerHTML = initialAmountAndCurrency;
-    updateTitle(convertedAmount, amount, from, to);
+    value = amount / 23000;
   }
+  const convertedAmount = formatCurrency(to, value);
+  document.getElementById("converted-amount").innerHTML = convertedAmount;
+  document.getElementById("start-amount").innerHTML = initialAmountAndCurrency;
+  updateTitle(convertedAmount, amount, from, to);
 }
 
 function reverse() {
