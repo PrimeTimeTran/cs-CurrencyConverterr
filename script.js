359f742c3f107371e83dccd7001a0cc004464158
Loi Van Tran <datloiboi@gmail.com>
2020-10-30 14:38:16 +0700

    Add currenecy formatting

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function formatCurrency(currency, value) {
   const formatter = new Intl.NumberFormat(currency, {
-    currency: currency,
-    style: "currency"
+    currency,
+    style: "currency",
   });
   return formatter.format(value);
 }
@@ -20,7 +20,10 @@ function updateTitle(converted, value, from, to) {
 
 function convertCurrency() {
   const [from, to] = getFromTo();
-  const amount = document.getElementById("amount").value;
+  const amount = document
+    .getElementById("amount")
+    .value.split(".")[0]
+    .replace(/\D/g, "");
   const initialAmountAndCurrency = `${amount} ${from} =`;
 
   let value;
@@ -45,3 +48,52 @@ function reverse() {
 }
 
 document.getElementById("reverse").addEventListener("click", convertCurrency);
+
+document.getElementById("amount").addEventListener("keyup", function (e) {
+  const [from] = getFromTo();
+  var formatter = new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: from,
+  });
+  const value = e.target.value.split(".")[0].replace(/\D/g, "");
+  const isNumber = isFinite(e.key);
+  if (isNumber) {
+    const go = formatter.format(value);
+    document.getElementById("amount").value = go;
+  }
+
+  function setCaretPosition() {
+    var el = document.getElementById("amount");
+    const la = e.target.value.split(".")[0];
+    const caretPos = la.length;
+
+    el.value = el.value;
+    if (el !== null) {
+      if (el.createTextRange) {
+        var range = el.createTextRange();
+        range.move("character", caretPos);
+        range.select();
+        return true;
+      } else {
+        if (el.selectionStart || el.selectionStart === 0) {
+          el.focus();
+          el.setSelectionRange(caretPos, caretPos);
+          return true;
+        } else {
+          el.focus();
+          return false;
+        }
+      }
+    }
+  }
+  if (from === "USD") {
+    setCaretPosition();
+  }
+
+  const shouldReFormat = e.key === "Enter" || e.key == "Backspace";
+  if (shouldReFormat) {
+    convertCurrency();
+    const go = formatter.format(value);
+    document.getElementById("amount").value = go;
+  }
+});
